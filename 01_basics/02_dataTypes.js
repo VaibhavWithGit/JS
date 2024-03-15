71c403a842dadf03bb1e16419589fe2c0e4e0c62
Vaibhav Kumbharikar <vaibhavkumbharikar@gmail.com>
2024-03-15 11:13:50 +0000

    DataType Update

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // Treat all JS code as newer version
+
+
+// DataTypes: 
+
+// number
+// BigInt
+// string
+// Boolean
+// undefined = var declare but not value
+// null = empty = mull is also object
+// symbol = unique
+// Object
